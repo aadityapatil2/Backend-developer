9e3525fe0a7504f51612bc1931e9584d3e3be652
aadityapatil12 <aadityapatil83089@gmail.com>
2022-08-06 22:18:04 +0530

    aasignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const abc = require('../introduction/intro')
+const getlogger = require('../logger/logger')
+const dateprint = require('../util/helper')
+const monthprint = require('../util/helper')
+const getinfo = require('../util/helper')
+const gettrim = require('../validetor/formatter')
+const getlower = require('../validetor/formatter')
+const getupper = require('../validetor/formatter')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
-    res.send('My second ever api!')
+
+    getlogger.Wellcome()
+
+    dateprint.printdate()
+
+    monthprint.printmonth()
+
+    getinfo.getBetchinfo()
+
+    gettrim.trim()
+
+    getlower.LowerCase()
+
+    getupper.uppercase()
+
+    res.send('Assignment of Nodejs!')
 });
 
 

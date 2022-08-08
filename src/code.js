afc3318c31c9bda84c857388e4093064ba4ffb8e
aadityapatil12 <aadityapatil83089@gmail.com>
2022-08-08 14:16:36 +0530

    postman assignment

---
+++ src/code.js
@@ -0,0 +1,11 @@
+// function repeatStr (n, s) {
+  
+//     if( n === "I" && s === "Hello"){
+//       console.log("IIIIII")
+//       console.log("HelloHelloHelloHelloHello")
+//     }
+//      return n ,s;
+//    }
+//    console.log(repeatStr("I","Hello"))
+const a = "heloo"
+console.log(a)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,31 +1,42 @@
 const express = require('express');
+const lodash = require('lodash')
 const abc = require('../introduction/intro')
-const getlogger = require('../logger/logger')
-const dateprint = require('../util/helper')
-const monthprint = require('../util/helper')
-const getinfo = require('../util/helper')
-const gettrim = require('../validetor/formatter')
-const getlower = require('../validetor/formatter')
-const getupper = require('../validetor/formatter')
+// const getlogger = require('../logger/logger')
+// const dateprint = require('../util/helper')
+// const monthprint = require('../util/helper')
+// const getinfo = require('../util/helper')
+// const gettrim = require('../validetor/formatter')
+// const getlower = require('../validetor/formatter')
+// const getupper = require('../validetor/formatter')
 const router = express.Router();
+const month = ["jan","feb","march","apr","may","jun","jul","aug","sept","oct","nov","des"]
+const odd = [1,3,5,7,9,11,13,15,17,19]
+const pair = [ ["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+const dublicatenumber = ([1],[1,2],[1,2,3],[1,2,3,4],[1,2,3,4,5])
+
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
-    abc.printName()
 
-    getlogger.Wellcome()
+console.log(lodash.chunk(month ,4))
+console.log(lodash.tail(odd ,9))
+console.log(lodash.fromPairs(pair))
+console.log(lodash.union(dublicatenumber))
+    // abc.printName()
 
-    dateprint.printdate()
+    // getlogger.Wellcome()
 
-    monthprint.printmonth()
+    // dateprint.printdate()
 
-    getinfo.getBetchinfo()
+    // monthprint.printmonth()
 
-    gettrim.trim()
+    // getinfo.getBetchinfo()
 
-    getlower.LowerCase()
+    // gettrim.trim()
 
-    getupper.uppercase()
+    // getlower.LowerCase()
+
+    // getupper.uppercase()
 
     res.send('Assignment of Nodejs!')
 });

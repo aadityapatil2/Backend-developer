17012d30b189417b56e47469b273b5f0bb27249c
aadityapatil12 <aadityapatil83089@gmail.com>
2022-08-10 09:19:13 +0530

    missing number

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const lodash = require('lodash')
-const abc = require('../introduction/intro')
 
 const router = express.Router();
-const month = ["jan","feb","march","apr","may","jun","jul","aug","sept","oct","nov","des"]
-const odd = [1,3,5,7,9,11,13,15,17,19]
-const pair = [ ["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
-const dublicatenumber = ([1],[1,2],[1,2,3],[1,2,3,4],[1,2,3,4,5])
+
+// ==========================sol1=============================
+router.get('/sol1', function(req, res){
+
+  let arr= [1,2,3,5,6,7]
+ 
+  let total = 0;
+  for (var i in arr) {
+      total += arr[i];
+  }
+
+  let lastDigit= arr.pop()
+  let consecutiveSum= lastDigit * (lastDigit+1) / 2
+  let missingNumber= consecutiveSum - total
+
+  res.send(  { data: missingNumber  }  );
+
+})
+// =========================sol2================================
+
+router.get('/sol2',function(req, res){
 
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
+  let arr= [33, 34, 35, 37, 38]
+  let len= arr.length
 
-console.log(lodash.chunk(month ,4))
-console.log(lodash.tail(odd ,9))
-console.log(lodash.fromPairs(pair))
-console.log(lodash.union(dublicatenumber))
+  let total = 0;
+  for (var i in arr) {
+      total += arr[i];
+  }
 
-
-    res.send('Assignment of Nodejs!')
+  let firstDigit= arr[0]
+  let lastDigit= arr.pop()
+  let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+  let missingNumber= consecutiveSum - total
+ 
+  res.send(  { data: missingNumber  }  );
 });
 
-
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-})
-
-router.get('/give-me-students-data',function(req, res){
-
-})
 module.exports = router;
 // adding this comment for no reason
